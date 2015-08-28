82799027f824900ef25c21a53dd15ce7004e69f4
xuyan1 <xuy217@lehigh.edu>
2015-08-28 13:48:40 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////
+//Xu Yan
+//lab01-HelloWorld
+//Hello World Java Program
+//
+// the rest is not an exact copy from the instructions on Lab01.docx found on coursesite
+//
+// the program will be compiled by the command
+//      javac HelloWorld.java
+// the program will be run by the command
+//      java HelloWorld
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+public static void main(String[] args) {
+
+// printing the statement "Hello World!" with a box
+System.out.println("####################");
+System.out.println("#                  #");
+System.out.println("#   Hello World!   #");
+System.out.println("#                  #");
+System.out.println("####################");
+System.out.println("test");
+
+//test/testtest
+//these two brackets after this line concludes the program
+}
+}
