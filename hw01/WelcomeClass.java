01d9bb3c6463273e0c7d270e7b309086dfb2a573
xuyan1 <xuy217@lehigh.edu>
2015-08-29 18:18:40 +0000

    First Homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+// Xu Yan
+// 8/29/2015
+// CSE 002 HW#1
+
+// This defines the class
+
+public class WelcomeClass{
+    
+// This adds the main method
+
+public static void main(String[] args) {
+
+// These lines will print the statement
+
+System.out.println("     -----------      ");
+System.out.println("     | WELCOME |      ");
+System.out.println("     -----------      ");
+System.out.println("   ^  ^  ^  ^  ^  ^   ");
+System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\  ");
+System.out.println(" |-X--U--Y--2--1--7-| ");
+System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /  ");
+System.out.println("   v  v  v  v  v  v   ");
+System.out.println("I stayed at Lehigh over the summer and researched catalysts.");
+System.out.println("I met a lot of people and learned a lot of lab skills.");
+
+// This concludes the program
+}
+}
